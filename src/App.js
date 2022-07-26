fc6c2050e78a10bb67ef0d07558ef9267d5a4026
Raunak Sharma <98183441+Sharma572@users.noreply.github.com>
2022-07-26 22:06:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 import Props_file from "/Props_file";
 export default function App() {
+  const [Song, setData] = useState("Wanna Know my favourite song");
   return (
     <div className="App">
       <h1>My favourite Singer</h1>
       <h2>"Music is the shorthand of emotion."</h2>
-      <Props_file name="Arijit Singh " reason="Romantic" />
-      <Props_file name="Lata Mangeshkar" reason="Melodious" />
+      <Props_file name="Arijit Singh " reason={"Romantic"} Song={Song} />
+
+      <h4 style={{ color: "darkred" }}>{Song}</h4>
+      <button
+        onClick={() => {
+          setData("Shayad by Arijit Sing and Tere Bina Zindagi Se by Lata Ji");
+        }}
+      >
+        favourite song
+      </button>
     </div>
   );
 }
